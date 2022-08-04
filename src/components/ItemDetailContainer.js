7f80cdfe8232a04e016e46a4d84689d95455fe0a
Mariel Sanchez <mariel.agustina.sanchez@gmail.com>
2022-08-03 23:51:17 -0300

    firebase

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react"
 import {useParams} from "react-router-dom"
+import {getFirestore, doc, getDoc} from 'firebase/firestore'
 import ItemDetail from "./ItemDetail"
 import {productos} from "./mock/productos"
 
@@ -7,20 +8,27 @@ export const ItemDetailContainer = () => {
   const [item, setItem] = useState({});
   const {id} = useParams();
 
-  const traerItemPorId = () => {
+ /* const traerItemPorId = () => {
     return new Promise((resolve)=>{
       setTimeout(()=>{
         resolve(productos.find(obj => obj.id == id));
       }, 1000)
     });
   };
-
-  useEffect(()=>{
+ 
+   useEffect(()=>{
     traerItemPorId().then((respuesta)=>{
       setItem(respuesta)
     });
   },[]);
+*/
 
+useEffect (() => {
+  const querydb = getFirestore();
+  const queryDoc = doc(querydb, 'products','p1ZYMyYktGV9gwKISayR p1ZYMyYktGV9gwKISayR')
+  getDoc(queryDoc)
+  .then(res => setItem({id: res.id, ...res.item() }))
+}, [])
 
   return (
     <ItemDetail item={item}/>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcQgtcBTivuNIQX23xzz1WYzsokVGh0ZQ",
+  authDomain: "proyecto-ecommerce-1c48d.firebaseapp.com",
+  projectId: "proyecto-ecommerce-1c48d",
+  storageBucket: "proyecto-ecommerce-1c48d.appspot.com",
+  messagingSenderId: "269959679491",
+  appId: "1:269959679491:web:63ae102847491676940836",
+  measurementId: "G-92X48T0P7W"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
